0f7070f3e68a1a45ab6da190466ebe9fbb990dc2
aakatev <aakatev@mail.sfsu.edu>
2019-07-05 06:26:15 +0000

    CORS added

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app/app.module';
 import { APP_HOST, APP_PORT } from './app/app.config.json';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   await app.listen(APP_PORT, APP_HOST);
 }
 bootstrap();
